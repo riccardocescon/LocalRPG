a4c0615d167b853258618749a4600a8bebcb6435
riccardocescon <36532502+riccardocescon@users.noreply.github.com>
2019-10-22 18:41:10 +0200

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public static Controller instance;
+
+    public Rigidbody2D rb;
+    private Animator anim;
+    private int speed = 3;
+    private float moveInput = 0f;
+    private float distToGround;
+
+
+    private void Awake() {
+        if(instance == null) instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+
+        anim = GetComponent<Animator>();
+
+        distToGround = GetComponent<Collider2D>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //#region JoystickLeftStick
+            //Jump on ColliderController script
+            if(Input.GetAxis("LeftStickHorizontal2") > 0.5f){
+            moveInput =Input.GetAxis("LeftStickHorizontal2");
+            anim.SetBool("MoveRight", true);
+            } 
+            else if(Input.GetAxis("LeftStickHorizontal2") < -0.5f){
+                moveInput = Input.GetAxis("LeftStickHorizontal2");
+                anim.SetBool("MoveRight", false);
+            }
+            else{
+                moveInput = 0f;
+                anim.SetBool("MoveRight", false);
+            }
+        //#endregion
+    
+        //#region JoystickButtons
+            if(Input.GetButtonDown("AButton2")){
+                Player.instance.Attack();
+            }
+        //#endregion
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+
+}
---
+++ Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    #region Player Data
+        public string name;
+        public float health;
+        public float power;
+        public float speed;
+        public float speedAttack;
+        public int xp;
+        public int mana;
+        public float armor;
+        public string lastClassUsed;
+        public int lvl;
+        public string classUnlocked;
+        
+    #endregion
+
+    public float startTimeBtwAttack;
+    private float timeBtwAttacks;
+
+
+
+    private void Awake() {
+        if(instance == null)instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack(){
+        if(timeBtwAttacks <= 0){
+            Debug.Log("Attack");
+                //Animation
+                //Togli vita al bersagio
+            timeBtwAttacks = startTimeBtwAttack;
+        }else{
+            timeBtwAttacks -= Time.deltaTime;
+        }
+    }
+}
