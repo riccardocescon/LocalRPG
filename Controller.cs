ca6f1334d483923bd3dbb628425e5c32a594bfab
riccardocescon <36532502+riccardocescon@users.noreply.github.com>
2019-10-23 21:48:14 +0200

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public static Controller instance;
+
+
+
+
+    private void Awake() {
+        if(instance == null) instance = this;
+    }
+
+
+    void Update()
+    {
+        #region JoystickLeftStick
+            //Jump on ColliderController script
+            if(Input.GetAxis("LeftStickHorizontal2") > 0.5f){
+            //PlayerAnimation.instance.SetSomething("LeftStickHorizontal2");
+            //PlayerAnimation.instance.moveInput =Input.GetAxis("LeftStickHorizontal2");
+            //anim.SetBool("MoveRight", true);
+            } 
+            else if(Input.GetAxis("LeftStickHorizontal2") < -0.5f){
+                //PlayerAnimation.instance.moveInput = Input.GetAxis("LeftStickHorizontal2");
+                //anim.SetBool("MoveRight", false);
+            }
+            else{
+                //PlayerAnimation.instance.moveInput = 0f;
+                //anim.SetBool("MoveRight", false);
+            }
+        #endregion
+    
+        #region JoystickButtons
+            if(Input.GetButtonDown("AButton2")){
+              //  Player.instance.Attack();
+            }
+        #endregion
+    }
+
+
+
+
+}
---
+++ Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    #region Player Data
+        public string name;
+        public float health;
+        public float power;
+        public float speed;
+        public float speedAttack;
+        public int xp;
+        public int mana;
+        public float armor;
+        public string lastClassUsed;
+        public int lvl;
+        public string classUnlocked;
+        public int JoystickNum;
+        
+    #endregion
+
+    #region PhysicsVariable
+        public Rigidbody2D rb;
+
+    #endregion
+
+    #region Time_Variable
+        public float startTimeBtwAttack;
+        private float timeBtwAttacks;
+    #endregion
+
+
+
+    private void Awake() {
+        if(instance == null)instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+
+        PlayerData pg = SaveSystem.GetPlayer(1);    //1 dovrebbe essere il numero del joystick. Trova il modo per riconoscere che joystick sta usando il giocatore
+        name = pg.name;                             //prova a loaddarli 1 all volta dal primo, e dopo averlo loaddato carichi il giocatore, in questo modo dovrebbe andare
+        health = pg.health;
+        power = pg.power;
+        speed = pg.speed;
+        speedAttack = pg.speedAttack;
+        xp = pg.xp;
+        mana = pg.mana;
+        armor = pg.armor;
+        lastClassUsed = pg.lastClassUsed;
+        lvl = pg.lvl;
+        classUnlocked = pg.classUnlocked;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack(){
+        if(timeBtwAttacks <= 0){
+            Debug.Log("Attack");
+                //Animation
+                //Togli vita al bersagio
+            timeBtwAttacks = startTimeBtwAttack;
+        }else{
+            timeBtwAttacks -= Time.deltaTime;
+        }
+    }
+
+    private void SetPlayer(){
+        
+    }
+}
